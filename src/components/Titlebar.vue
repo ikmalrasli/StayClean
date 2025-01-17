73c4bd7c2d725154a48b96d1e6595f71fa311c60
eemauuu <ikmalrasli@gmail.com>
2025-01-17 11:30:47 +0800

    title bar has history and back button on stats page

--- src/components/Titlebar.vue
+++ src/components/Titlebar.vue
@@ -1,16 +1,25 @@
 <!-- src/components/Titlebar.vue -->
 <template>
   <header class="w-full min-h-14 py-2 px-4 flex items-center relative">
+    <button v-if="title === 'History'"
+    class="absolute left-4 top-1/2 transform -translate-y-1/2 text-lg text-primary hover-card rounded-full"
+    @click="toStats">
+      <i class="fa-solid fa-chevron-left"></i>
+    </button>
     <h1 class="text-lg font-bold text-primary absolute left-1/2 transform -translate-x-1/2">
       {{ title }}
     </h1>
-    <button v-if="title === 'Stats'" class="absolute right-4 top-1/2 transform -translate-y-1/2 text-lg text-primary hover-card rounded-full">
+    <button v-if="title === 'Stats'"
+    class="absolute right-4 top-1/2 transform -translate-y-1/2 text-lg text-primary hover-card rounded-full"
+    @click="toHistory">
       <i class="fa-solid fa-clock-rotate-left"></i>
     </button>
   </header>
 </template>
 
 <script>
+import { EventBus } from '../eventBus';
+
 export default {
   props: {
     title: {
@@ -18,7 +27,14 @@ export default {
       required: true,
     },
   },
-  emits: ['toggleTheme'], // Emit to toggle light/dark mode
+  methods: {
+    toHistory(){
+      EventBus.emit('navigateHistory');
+    },
+    toStats(){
+      EventBus.emit('navigateStats');
+    }
+  }
 };
 </script>
 
