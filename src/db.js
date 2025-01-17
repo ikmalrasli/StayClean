ccfe5cb693254df6d0a3480fea965235c7273272
eemauuu <ikmalrasli@gmail.com>
2025-01-17 11:27:53 +0800

    gain EXP every 24 Hrs and ResetHabit function

--- src/db.js
+++ src/db.js
@@ -7,15 +7,14 @@ const db = new Dexie('stayclean_DB');
 db.version(1).stores({
   habit: 'id, name, dateStart, color, xpLevel, currentExp, maxExp, streak, battles, nextExpGainTime',
   exp: '++id, time, exp, type',
-  trials: '++id, trial, level, exp, streak, dateEnded',
+  trials: '++id, trial, xpLevel, streak, battles, dateStarted, dateEnded, reason',
 });
 
 export default db;
 
 export async function setHabit(name, dateStart, color) {
   try {
-    // const nextExpGainTime = new Date(dateStart).getTime() + 24 * 60 * 60 * 1000; // First gain after 24 hours
-    const nextExpGainTime = new Date(dateStart).getTime() + 60 * 1000; // First gain after 1 minute
+    const nextExpGainTime = new Date(dateStart).getTime() + 24 * 60 * 60 * 1000; // First gain after 24 hours
     await db.habit.put({
       id: 1,
       name,
@@ -41,5 +40,46 @@ export async function deleteHabit() {
     console.log('Habit deleted successfully!');
   } catch (error) {
     console.error('Failed to delete habit:', error);
+  }
+}
+
+export async function resetHabit(dateEnded, reason) {
+  const habit = await db.habit.get(1); // Get the current habit
+  if (!habit) {
+    console.error("No habit found to reset.");
+    return;
+  }
+
+  try {
+    if (!dateEnded || !reason) {
+      console.error("Invalid dateEnded or reason.");
+      return;
+    }
+
+    const trialCount = await db.trials.count();
+
+    await db.trials.add({
+      trial: trialCount + 1, 
+      xpLevel: habit.xpLevel,
+      streak: habit.streak,
+      battles: habit.battles,
+      dateStarted: habit.dateStart,
+      dateEnded: dateEnded,
+      reason: reason,
+    });
+
+    await db.habit.update(1, {
+      dateStart: dateEnded,
+      xpLevel: 1,
+      currentExp: 0,
+      maxExp: 100,
+      streak: 0,
+      battles: 0,
+      lastResetDate: dateEnded
+    });
+
+    console.log("Habit successfully reset.");
+  } catch (error) {
+    console.error("Failed to reset habit:", error);
   }
 }
