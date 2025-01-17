8c96eae14ebbaac6995a38172c60806f098511b2
eemauuu <ikmalrasli@gmail.com>
2025-01-17 11:31:31 +0800

    new page added (history), able to receive eventBus from indirect components

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <!-- src/views/Dashboard.vue -->
 <template>
-  <div class="flex flex-col h-screen text-primary">
+  <div class="flex flex-col h-dvh text-primary">
     <!-- Title Bar -->
     <Titlebar :title="pageTitles[currentPage]" @toggleTheme="toggleTheme" />
 
@@ -20,9 +20,11 @@ import Navbar from '../components/Navbar.vue';
 import Home from './pages/Home.vue';
 import Stats from './pages/Stats.vue';
 import Settings from './pages/Settings.vue';
+import History from './pages/History.vue';
+import { EventBus } from '../eventBus';
 
 export default {
-  components: { Titlebar, Navbar, Home, Stats, Settings },
+  components: { Titlebar, Navbar, Home, Stats, Settings, History },
   data() {
     return {
       currentPage: 'home', // Default page
@@ -30,6 +32,7 @@ export default {
         home: 'Home',
         stats: 'Stats',
         settings: 'Settings',
+        history: 'History',
       },
     };
   },
@@ -37,6 +40,14 @@ export default {
     // Load saved theme from localStorage or default to 'light'
     const savedTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.classList.add(savedTheme);
+
+    // Listen for navigate events from EventBus
+    EventBus.on('navigateHistory', () => {
+      this.currentPage = 'history';
+    });
+    EventBus.on('navigateStats', () => {
+      this.currentPage = 'stats';
+    });
   },
   methods: {
     navigate(page) {
